68981ec43a53f90410f915bbeac00796a88a6f9b
oluwaspecial05 <folaemmmy@gmail.com>
2022-10-13 21:28:08 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include (stdio.h)
-int main()
+#include<stdio.h>
+/**
+ * main-welcome to c
+ *
+ * Return:software engineering
+ */
+ int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
 }
