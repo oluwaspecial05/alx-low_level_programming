e647642ea377524c38c099c82157238bf9c15a50
oluwaspecial05 <folaemmmy@gmail.com>
2022-10-14 02:41:25 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return:software engineering
  */
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
