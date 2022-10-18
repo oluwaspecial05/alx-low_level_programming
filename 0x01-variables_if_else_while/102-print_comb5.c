7cfaf433195040249064ea13fec726b1ab377135
oluwaspecial05 <folaemmmy@gmail.com>
2022-10-18 04:55:13 -0700

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,27 +12,27 @@ int main(void)
 	{
 		for (b = 48; b <= 57; b++)
 		{
-			for (c = 48; b <= 57; c++)
+			for (c = 48; c <= 57; c++)
 			{
 				for (d = 48; d <= 57; d++)
 				{
-					if (((c + d) > (a + b) && c >= a) || a < c)
-					{
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
-						if (a + b + c + d == 227 && a == 57)
-						{
-						break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+				if (((c + d) > (a + b) && c >= a) || a < c)
+				{
+				putchar(a);
+				putchar(b);
+				putchar(' ');
+				putchar(c);
+				putchar(d);
+				if (a + b + c + d == 227 && a == 57)
+				{
+				break;
+				}
+				else
+				{
+				putchar(',');
+				putchar(' ');
+				}
+				}
 				}
 			}
 		}
