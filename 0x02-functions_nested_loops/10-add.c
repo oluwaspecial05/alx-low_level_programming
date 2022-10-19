a5b06b78d92c1164aac533d081c3f0bf9e00bf99
oluwaspecial05 <folaemmmy@gmail.com>
2022-10-19 08:46:38 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @x1: The first number to be entered
+ * @x2: The second number to be entered
+ *
+ * Return: Integer value
+ */
+int add(int x1, int x2)
+{
+	return (x1 + x2);
+}
