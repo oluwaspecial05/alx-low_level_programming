e4baaa68ed9d4d10d87e9175174241b931e58584
oluwaspecial05 <folaemmmy@gmail.com>
2022-10-20 15:56:59 -0700

    update

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/* putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
